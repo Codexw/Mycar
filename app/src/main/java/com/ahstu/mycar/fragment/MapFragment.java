721cc1a7cd117a53515930460aa4eead43ab8a63
redowu <redowu@163.com>
2016-05-05 16:32:41 +0800

    吴天洛，完成方向传感器功能

--- app/src/main/java/com/ahstu/mycar/fragment/MapFragment.java
+++ app/src/main/java/com/ahstu/mycar/fragment/MapFragment.java
@@ -9,15 +9,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.ahstu.mycar.R;
+import com.ahstu.mycar.ui.MyOrientationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 
@@ -31,11 +35,19 @@ public class MapFragment extends Fragment implements View.OnClickListener {
     private Button btn_map_normal;
     private Button btn_map_site;
     private Button btn_map_traffic;
+    private Button btn_map_mylocation;
 
     //定位相关变量
     private LocationClient mLocationClient;
     private MyLocationListener myLocationListener;
     private boolean isFirstIn = true;
+    private double mLatitude;
+    private double mLongitude;
+
+    //自定义定位图标
+    private BitmapDescriptor mbitmapDescriptor;
+    private MyOrientationListener mMyOrientationListener;
+    private float mCurrentX;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,13 +73,23 @@ public class MapFragment extends Fragment implements View.OnClickListener {
         option.setIsNeedAddress(true);
         option.setOpenGps(true);
         option.setScanSpan(1000);
-
+        mLocationClient.setLocOption(option);//将上面option中的设置加载
+        //初始化图标
+        mbitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.map_my_location_icon);
+        mMyOrientationListener = new MyOrientationListener(getActivity());
+        mMyOrientationListener.setmOnOrientationListener(new MyOrientationListener.OnOrientationListener() {
+            @Override
+            public void onOrientationChanged(float x) {
+                mCurrentX = x;
+            }
+        });
     }
 
     private void initClick(View view) {
         btn_map_normal.setOnClickListener(this);
         btn_map_site.setOnClickListener(this);
         btn_map_traffic.setOnClickListener(this);
+        btn_map_mylocation.setOnClickListener(this);
     }
 
     private void ininView(View view) {
@@ -79,16 +101,20 @@ public class MapFragment extends Fragment implements View.OnClickListener {
         btn_map_normal = (Button) view.findViewById(R.id.btn_map_normal);
         btn_map_site = (Button) view.findViewById(R.id.btn_map_site);
         btn_map_traffic = (Button) view.findViewById(R.id.btn_map_traffic);
+        btn_map_mylocation = (Button) view.findViewById(R.id.btn_map_mylocation);
     }
 
     //软件开启时判断滴入是否打开，没有打开，则打开
     @Override
     public void onStart() {
         super.onStart();
+        //开启定位
         mBaiduMap.setMyLocationEnabled(true);
         if (!mLocationClient.isStarted()) {
             mLocationClient.start();
         }
+        //开启方向传感器
+        mMyOrientationListener.start();
     }
 
     @Override
@@ -118,8 +144,12 @@ public class MapFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onStop() {
         super.onStop();
+        //停止地图定位
         mBaiduMap.setMyLocationEnabled(false);
         mLocationClient.stop();
+
+        //停止方向传感器
+        mMyOrientationListener.stop();
     }
 
     @Override
@@ -136,9 +166,15 @@ public class MapFragment extends Fragment implements View.OnClickListener {
                     mBaiduMap.setTrafficEnabled(false);
                     btn_map_traffic.setText("实时交通(off)");
                 } else {
+
                     mBaiduMap.setTrafficEnabled(true);
                     btn_map_traffic.setText("实时交通(on)");
                 }
+                break;
+            case R.id.btn_map_mylocation:
+                LatLng latLng = new LatLng(mLatitude, mLongitude);
+                MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+                mBaiduMap.animateMapStatus(msu);
                 break;
         }
     }
@@ -149,15 +185,20 @@ public class MapFragment extends Fragment implements View.OnClickListener {
         @Override
         public void onReceiveLocation(BDLocation location) {
             MyLocationData data = new MyLocationData.Builder()//
+                    .direction(mCurrentX)//
                     .accuracy(location.getRadius())//
                     .latitude(location.getLatitude())//
                     .longitude(location.getLongitude())//
                     .build();
 
-//            MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL)
             mBaiduMap.setMyLocationData(data);
+            MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL, true, mbitmapDescriptor);
+            mBaiduMap.setMyLocationConfigeration(config);
+            mLatitude = location.getLatitude();
+            mLongitude = location.getLongitude();
+
             if (isFirstIn) {
-                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                LatLng latLng = new LatLng(mLatitude, mLongitude);
                 MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
                 mBaiduMap.animateMapStatus(msu);
                 isFirstIn = false;
