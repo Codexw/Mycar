f63640c6d2e6359d5d1b3a5a7e369afc52a39f3c
xuning123 <1021301406@qq.com>
2016-06-05 16:08:12 +0800

    徐宁添加缓存机制

--- app/src/main/java/com/ahstu/mycar/fragment/MeInfoFragment.java
+++ app/src/main/java/com/ahstu/mycar/fragment/MeInfoFragment.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -138,8 +139,13 @@ public class MeInfoFragment extends Fragment {
                 ed.commit();
                 
                 getActivity().deleteDatabase("node.db");
-                if (mService.isPlay()) {
-                    mService.pausePlay();
+                try {
+                    if (mService.isPlay()) {
+                        mService.pausePlay();
+                    }
+                } catch (Exception e) {
+                    Log.e("music", ">>>>>>>>>>>>>>>>>>>>退出异常<<<<<<<<<<<<<<<");
+                    
                 }
                 startActivity(new Intent(getActivity(), LoginActivity.class));
             }
